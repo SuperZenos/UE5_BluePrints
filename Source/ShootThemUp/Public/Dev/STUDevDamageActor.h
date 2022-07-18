ca26481588860e7900ebe7baf10a6105f8c340d6
Zenos <zzy_hust@qq.com>
2022-07-18 12:06:01 +0800

    DamageActor

---
+++ Source/ShootThemUp/Public/Dev/STUDevDamageActor.h
@@ -0,0 +1,37 @@
+// Shoot Them Up Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "STUDevDamageActor.generated.h"
+
+UCLASS()
+class SHOOTTHEMUP_API ASTUDevDamageActor : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    // Sets default values for this actor's properties
+    ASTUDevDamageActor();
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+    USceneComponent* SceneComponent;
+
+protected:
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float Radius = 300.0f;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FColor SphereColor = FColor::Blue;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float Damage = 10.0f;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    bool DoFullDamage = true;
+
+public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+};
