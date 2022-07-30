987d1016f6f63dd6219cd7d5c2811fb7296ea818
Zenos <zzy_hust@qq.com>
2022-07-30 17:25:20 +0800

    Projectile

--- Source/ShootThemUp/Private/Weapon/STULauncherWeapon.cpp
+++ Source/ShootThemUp/Private/Weapon/STULauncherWeapon.cpp
@@ -1,5 +1,20 @@
 // Shoot Them Up Game. All Rights Reserved
 
-
 #include "Weapon/STULauncherWeapon.h"
+#include "Weapon/STUProjectile.h"
+#include "Kismet/GameplayStatics.h"
 
+void ASTULauncherWeapon::StartFire()
+{
+    MakeShot();
+}
+
+void ASTULauncherWeapon::MakeShot()
+{
+    const FTransform SpawnTransform(FRotator::ZeroRotator, GetMuzzleWorldLocation());
+    auto Projectile = UGameplayStatics::BeginDeferredActorSpawnFromClass(GetWorld(), ProjectileClass, SpawnTransform);
+    // Set projectile params
+    
+    UGameplayStatics::FinishSpawningActor(Projectile, SpawnTransform);
+    //GetWorld()->SpawnActor<ASTUProjectile>(ProjectileClass, SpawnTransform);
+}
--- Source/ShootThemUp/Public/Weapon/STULauncherWeapon.h
+++ Source/ShootThemUp/Public/Weapon/STULauncherWeapon.h
@@ -6,12 +6,19 @@
 #include "Weapon/STUBaseWeapon.h"
 #include "STULauncherWeapon.generated.h"
 
-/**
- * 
- */
+class ASTUProjectile;
+
 UCLASS()
 class SHOOTTHEMUP_API ASTULauncherWeapon : public ASTUBaseWeapon
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+
+public:
+    virtual void StartFire() override;
+
+protected:
+    UPROPERTY(EditDefaultsOnly, Category = "WeaponParams")
+    TSubclassOf<ASTUProjectile> ProjectileClass;
+
+    virtual void MakeShot() override;
 };
